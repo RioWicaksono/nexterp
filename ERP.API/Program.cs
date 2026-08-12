91497e016229ac11b137d086ffd2a2b333179dc3
SeVeN- <admin@nexterp.com>
2026-08-12 22:33:11 +0700

    chore: remove debug endpoint, cleanup

--- ERP.API/Program.cs
+++ ERP.API/Program.cs
@@ -245,8 +245,6 @@ app.UseAuthentication();
 app.UseRateLimiting();
 app.UseAuthorization();
 
-app.MapGet("/test", () => Results.Ok(new { message = "Routing works!", timestamp = DateTime.UtcNow }));
-
 app.MapControllers();
 
 // Health check endpoints
